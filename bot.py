309eea8094e6e8acdf4588cccec64fcd2988d65a
iMultiPlay <imultiplay123@gmail.com>
2014-03-10 13:24:48 -0400

    Bot can post with proper formatting
    need to build a system that properly queues quotes and possibly generate
    random ones

--- bot.py
+++ bot.py
@@ -28,13 +28,14 @@ class BashOrgBot(object):
         
 
         #First quote stuff (for testing purposes)
-        self.quote0 = self.quotes[45][0]
-        self.ref0 = self.quotes[45][1]
-        self.upvotes0 = self.quotes[45][2]
+        self.quote0 = self.quotes[33][0]
+        self.ref0 = self.quotes[33][1]
+        self.upvotes0 = self.quotes[3][2]
         print(self.quote0)
         print(self.ref0)
         print(self.upvotes0)
-        
+
+        self.submit_post(self.quote0, self.ref0, self.upvotes0)
     def _read_config(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
@@ -56,7 +57,7 @@ class BashOrgBot(object):
         del data[0] #cut off banner html on top of page
         data[-1] = data[-1][:data[-1].find('</td')] #There's some more html at the bottom, cutting it off
 
-        #Make some functions to condense code a bit
+        #Make some functions to condense code a bit:
         #Cut string between points by finding first part and ending with a number
         sortquote = lambda listx, x, y: listx[listx.find(x)+len(x):y]
         #Pull all info between two substrings
@@ -81,18 +82,17 @@ class BashOrgBot(object):
             quotes.append((holdquote, holdref, holdnum))
         return quotes                  
     
-    
     def submit_post(self, quote, refnum, upvotes):
-        quotetext = textwrap.wrap(quote, 100)[0]+'...'
-        
+        quotetext = quote[quote.find('<'):quote.find('\n')]
+        if len(quotetext) >= 97:
+            quotetext = textwrap.wrap(quotetext, 100)[0]+'...'
+        else:
+            quotetext = quotetext + '  '
+        bodytext = quote + '  \n  [Source](http://bash.org/?%s' % (refnum[1:]+')')
         title = "%s %s (%s)" % (refnum, quotetext, upvotes)
-        ##I haven't yet added the code to submit, I've done that in a terminal
-        ##This is what it would look like:
-        #r.submit('BashDotOrg', 'test post 3 please ignore', url='http://bash.org/?400459', raise_captcha_exception=True)
-        #
-        ##Which has only raised the: "Would you like to try that again?" error
-    def hi(self):
-        print('hi')
+        
+        self.r.submit('BashDotOrg', title, bodytext)
+        
 BashOrgBot()
 
     
