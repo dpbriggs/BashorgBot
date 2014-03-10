c2dbd80d920ba791f77a09f1fd0ddc670749af9e
iMultiPlay <imultiplay123@gmail.com>
2014-03-10 18:41:25 -0400

    Text grabbing and post submitting with wiki reference works
    Bot works as intended so far

--- bot.py
+++ bot.py
@@ -3,39 +3,45 @@ import configparser
 import textwrap
 import http.client
 import html.parser
-
+from time import sleep
+from random import randint
 
 class BashOrgBot(object):
     def __init__(self):
         #Read config file
         self._read_config()
-
+        
         #Variables for praw
         self.login_user = self.config['user']['login_user']
         self.login_pass = self.config['user']['login_pass']
         self.user_agent = self.config['user']['user_agent']
+        self.is_random = True if self.config['user']['mode'] == 'random' else False
+        self.time = int(self.config['user']['time_for_list_submit'])
         
         #Variables for getting html
         self.url = 'bash.org'
-        self.destination = '/?latest'
-        self.cutpoint = '<p class="quote">'
+        
         
         #Setup praw for later
         self.setup_praw()
 
+        #Get list of previously submitted numbers to avoid doubles
+        self.wiki_page = self._read_wiki()
         #Data from bash.org in form of quotes[quote_position][quote, refnum, upvotes/downvotes]
-        self.quotes = self.return_data(self.url, self.destination, self.cutpoint)
+        #And submit it
+        if self.is_random == True:
+            self.quotes = self.return_random_data()
+            self._submit_post(self.quotes[0], self.quotes[1], self.quotes[2])
+        else:
+            self.quotes = self.return_latest_data()
+            self.submit_list(self.time)
+
+      
+            
+                              
         
-
-        #First quote stuff (for testing purposes)
-        self.quote0 = self.quotes[33][0]
-        self.ref0 = self.quotes[33][1]
-        self.upvotes0 = self.quotes[3][2]
-        print(self.quote0)
-        print(self.ref0)
-        print(self.upvotes0)
-
-        self.submit_post(self.quote0, self.ref0, self.upvotes0)
+        
+        #self.submit_post(self.quote0, self.ref0, self.upvotes0)
     def _read_config(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
@@ -44,13 +50,29 @@ class BashOrgBot(object):
         self.r = praw.Reddit(user_agent=self.user_agent)
         self.r.login(self.login_user, self.login_pass)
         
+    def submit_list(self, time):
+        for i in range(0, len(self.quotes)):
+            if self.check_if_submitted(self.quotes[i][1]) != True:
+                self._submit_post(self.quotes[i][0], self.quotes[i][1], self.quotes[i][2])
+                print('Post submitted successfully!')
+                print('Waiting %s minutes...' %str(self.time))
+                sleep(time*60)
+                       
+    def check_if_submitted(self, ref_num):
+        if ref_num[1:] in self.wiki_page:
+            return True
+        else:
+            return False
+        
+    def return_latest_data(self):
+        destination = '/?latest'
+        cutpoint = '<p class="quote">'
 
-    def return_data(self, url, destination, cutpoint):
         #init HTMLParser
         h = html.parser.HTMLParser()
 
         #Download HTML page and make it workable
-        conn = http.client.HTTPConnection(url)
+        conn = http.client.HTTPConnection(self.url)
         conn.request("GET", destination) #Get the info
         res = conn.getresponse()
         data = str(res.read(), 'ascii').split(cutpoint) #Read data and cut it into bits
@@ -80,20 +102,75 @@ class BashOrgBot(object):
                 holdnum = '+' + holdnum
         
             quotes.append((holdquote, holdref, holdnum))
-        return quotes                  
+        return quotes
+
     
-    def submit_post(self, quote, refnum, upvotes):
-        quotetext = quote[quote.find('<'):quote.find('\n')]
+    def return_random_data(self):
+        #init HTMLParser
+        #print('started random data')
+        h = html.parser.HTMLParser()
+        
+            
+        ran_des = '/?random'
+        #Download HTML page and make it workable
+        conn = http.client.HTTPConnection(self.url)
+        conn.request("GET", ran_des) #Get the info
+        res = conn.getresponse()
+        data = str(res.read(), 'ascii').split('<p class="quote">')
+        del data[0]
+        #We only need one quote, pick a random one
+        single_quote = data[randint(0, len(data))]
+        #Cut string between points by finding first part and ending with a number
+        sortquote = lambda listx, x, y: listx[listx.find(x)+len(x):y]
+        #Pull all info between two substrings
+        sortrest = lambda listx, x, y: listx[listx.find(x)+len(x):listx.find(y)] 
+
+        #Grab quote and format it
+        holdquote = sortquote(single_quote, '</a></p><p class="qt">', -5)
+        holdquote = h.unescape(holdquote) #convert HTML parts to special characters
+        holdquote = holdquote.replace('<br />', '\n  ')
+
+        #Get reference number
+        holdref = sortrest(single_quote, '<b>', '</b>')
+
+        #Check if we've already used that quote
+        #If so redo everything
+        if self.check_if_submitted(holdref) == True:
+            self.return_random_data()
+
+        #Get upvotes / downvotes
+        holdnum = sortrest(single_quote, '</a>(', ')<a href=')
+        if holdnum[0] != '-':
+            holdnum = '+' + holdnum
+        
+        quote = [holdquote, holdref, holdnum]
+        return quote
+    
+    def _read_wiki(self):
+        #Convert string of numbers seperated by commas and spaces into a list of numbers (as str)
+        wiki_page = self.r.get_wiki_page('BashDotOrg', 'index').content_md.replace(' ', '').split(',')
+        return wiki_page
+    
+    def _write_wiki(self, ref_num):
+        #Convert list of numbers into a string
+        full_list = self.wiki_page
+        full_list.append(ref_num[1:])
+        join_list = ', '.join(full_list)
+        self.r.edit_wiki_page('BashDotOrg', 'index', join_list)
+        
+    def _submit_post(self, quote, ref_num, upvotes):
+        quotetext = quote[:quote.find('\n')]
         if len(quotetext) >= 97:
             quotetext = textwrap.wrap(quotetext, 100)[0]+'...'
         else:
             quotetext = quotetext + '  '
-        bodytext = quote + '  \n  [Source](http://bash.org/?%s' % (refnum[1:]+')')
-        title = "%s %s (%s)" % (refnum, quotetext, upvotes)
-        
-        self.r.submit('BashDotOrg', title, bodytext)
-        
-BashOrgBot()
+        bodytext = quote + '  \n  [Source](http://bash.org/?%s' % (ref_num[1:]+')')
+        title = "%s %s (%s)" % (ref_num, quotetext, upvotes)
 
+        #Submit thread
+        self.r.submit('BashDotOrg', title, bodytext)
+        #Record quote in wiki
+        self._write_wiki(ref_num)
+        
     
 
